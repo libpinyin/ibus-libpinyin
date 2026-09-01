0ac3b25da490833cbf9f047e6e69b96b916d4b0e
tuberry <17917040+tuberry@users.noreply.github.com>
2026-09-02 07:10:51 +0800

    Refactor get_chinese_non_math_num implementation
    Refactor get_chinese_non_math_num function to use gmatch for character processing.

--- lua/base.lua
+++ lua/base.lua
@@ -38,9 +38,15 @@ function get_chinese_math_num(num)
 end
 
 function get_chinese_non_math_num(num)
-	return tostring(num):gsub(".", function(ch)
-		return _CHINESE_DIGITS[tonumber(ch)]
-	end)
+  local ret = ""
+  for ch in tostring(num):gmatch(".") do
+	local ch = ch
+    if ch >= "0" and ch <= "9" then
+      ch = _CHINESE_DIGITS[tonumber(ch)]
+    end
+    ret = ret .. ch
+  end
+  return ret
 end
 
 function _verify_time(hour, minute)
